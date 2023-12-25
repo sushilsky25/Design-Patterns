1561b3f15123ab6156420df582a5897b9d4a8b06
sushilsky25 <31982301+sushilsky25@users.noreply.github.com>
2023-12-25 15:45:43 +0530

    Add files via upload
    Abstract Class

---
+++ Abstract/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Abstract
+{
+    abstract class Programming
+    {
+        public abstract void Developer();
+        public abstract void Rank();
+    }
+
+    abstract class HTML : Programming
+    {
+        public override void Developer()
+        {
+            Console.WriteLine("Sushil Yadav");
+        }
+    }
+
+    class Java : HTML
+    {
+        public override void Rank()
+        {
+            Console.WriteLine("1st");
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Programming h = new Java();
+            h.Developer();
+            h.Rank();
+        }
+    }
+}
