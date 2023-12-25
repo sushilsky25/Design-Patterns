a00c80ce420568118dceee1dce8b6f7d56d047d7
sushilsky25 <31982301+sushilsky25@users.noreply.github.com>
2023-12-25 17:16:06 +0530

    Add files via upload

---
+++ Interface/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Interface
+{
+    using System;
+    interface Customer
+    {
+        int AMT { get; } // public + static + final
+        void Purchase(); // public + abstract
+    }
+
+    class SellerSky : Customer
+    {
+        public int AMT { get; } = 5;
+
+        public void Purchase()
+        {
+            Console.WriteLine($"Customer needs {AMT} Kg rice");
+        }
+    }
+
+    class Check
+    {
+        public static void Main(string[] args)
+        {
+            Customer s = new SellerSky();
+            s.Purchase();
+            Console.WriteLine(((SellerSky)s).AMT);
+        }
+    }
+}
