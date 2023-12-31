9991e4e446a1ffe3d2516304958931219c9b2790
sushilsky25 <31982301+sushilsky25@users.noreply.github.com>
2023-12-31 19:05:32 +0530

    Add files via upload
    FactoryPattern

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    // The Product interface
+    public interface IFactory
+    {
+        void Drive(int miles);
+    }
+
+    // A ConcreteProduct class
+    public class Scooter : IFactory
+    {
+        public void Drive(int miles)
+        {
+            Console.WriteLine("Drive the Scooter : " + miles.ToString() + "km");
+        }
+    }
+
+    // A ConcreteProduct class
+    public class Bike : IFactory
+    {
+        public void Drive(int miles)
+        {
+            Console.WriteLine("Drive the Bike : " + miles.ToString() + "km");
+        }
+    }
+
+    // The Creator Abstract Class
+    public abstract class VehicleFactory
+    {
+        public abstract IFactory GetVehicle(string Vehicle);
+
+    }
+
+    // A ConcreteCreator class
+    public class ConcreteVehicleFactory : VehicleFactory
+    {
+        public override IFactory GetVehicle(string Vehicle)
+        {
+            switch (Vehicle)
+            {
+                case "Scooter":
+                    return new Scooter();
+                case "Bike":
+                    return new Bike();
+                default:
+                    throw new ApplicationException(string.Format("Vehicle '{0}' cannot be created", Vehicle));
+            }
+        }
+    }
+
+    // Factory Pattern
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VehicleFactory factory = new ConcreteVehicleFactory();
+
+            IFactory scooter = factory.GetVehicle("Scooter");
+            scooter.Drive(10);
+
+            IFactory bike = factory.GetVehicle("Bike");
+            bike.Drive(20);
+
+            Console.ReadKey();
+
+        }
+    }
+}
